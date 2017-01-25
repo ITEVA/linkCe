74d9aca2338c5e813fa3e86cd6fe4e661c96ec6b
Naldo Maranhão <regisousa7@gmail.com>
2017-01-25 11:53:06 -0300

    Mapa em contatos

--- Pages/contatos.php
+++ Pages/contatos.php
@@ -46,6 +46,7 @@
         </div>
         <div id="noteContato" class="">
             <img src="Images/noteContato.png">
+            <iframe id="mapaContato" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.7344767469835!2d-38.62594188464877!3d-3.8670369971871397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7531a85c6906b%3A0x47d9505e9af5ff10!2sR.+Quatorze%2C+660+-+Novo+Maracana%C3%BA%2C+Maracana%C3%BA+-+CE%2C+61905-570!5e0!3m2!1spt-BR!2sbr!4v1485352355232" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
 <!--
